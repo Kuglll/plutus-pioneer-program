73d0ccd8bc9aeeb5bf7b2fae358c3a7026772a72
Kuglll <mark.frelih@gmail.com>
2022-02-08 20:35:23 +0100

    [Homework4] Implement error handling in payContract

--- code/week04/src/Week04/Homework.hs
+++ code/week04/src/Week04/Homework.hs
@@ -10,7 +10,7 @@ module Week04.Homework where
 
 import Data.Aeson            (FromJSON, ToJSON)
 import Data.Functor          (void)
-import Data.Text             (Text)
+import Data.Text             (Text, unpack)
 import GHC.Generics          (Generic)
 import Ledger
 import Ledger.Ada            as Ada
@@ -30,8 +30,9 @@ payContract :: Contract () PaySchema Text ()
 payContract = do
     pp <- awaitPromise $ endpoint @"pay" return
     let tx = mustPayToPubKey (ppRecipient pp) $ lovelaceValueOf $ ppLovelace pp
-    void $ submitTx tx
+    Contract.handleError (\err -> Contract.logError $ "caught: " ++ unpack err) $ void $ submitTx tx
     payContract
+
 
 -- A trace that invokes the pay endpoint of payContract on Wallet 1 twice, each time with Wallet 2 as
 -- recipient, but with amounts given by the two arguments. There should be a delay of one slot
